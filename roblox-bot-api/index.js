3119cbc8e2177c7e50d6f8c58ff1dcf2154207d3
ɪᴋɢᴍᴏɴxʀ <imgmtt@gmail.com>
2026-07-30 18:06:53 -0600

    Update index.js

--- roblox-bot-api/index.js
+++ roblox-bot-api/index.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('¡API de Roblox conectada y funcionando!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
